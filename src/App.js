72a2429ce77bf41f33be8851cc3a96804996f96e
kudryk <kudryk@kth.se>
2021-04-16 13:20:10 +0200

    Revert "bara början"
    This reverts commit 33b5fb2ceaf4278e0f94737cfcffa4bab490abd2.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import { Main } from './features/main/Main';
 import './App.css';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <Main />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/features/counter/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-export function Main() {
-    return(
-        <div>
-            hej
-        </div>
-    )
-}
