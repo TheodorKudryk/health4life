7272c67bcca3d22687d0261f335cf403a6a77544
kudryk <kudryk@kth.se>
2021-04-20 14:17:33 +0200

    Main och login

---
+++ src/features/main/mainSlice.js
@@ -0,0 +1,13 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+export const slice = createSlice({
+    name: 'main',
+    initialState: [],
+    reducers: {
+
+    }
+
+    }
+);
+
+export default slice.reducer;
