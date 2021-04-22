5597f4dd17935df283546000357e9961d781afbf
SaraBertse <61227732+SaraBertse@users.noreply.github.com>
2021-04-22 14:38:44 +0200

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, createStore } from 'redux'
+import weightsReducer from '../features/profile/weightsSlice';
+import setHeightReducer from '../features/profile/heightSlice';
+import setAgeReducer from '../features/profile/ageSlice';
+import setSexReducer from '../features/profile/sexSlice';
+import setNameReducer from '../features/profile/nameSlice';
+import setActivityLevelReducer from '../features/profile/activityLevelSlice';
+import setGoalReducer from '../features/profile/goalSlice';
 import userReducer from '../features/login/userSlice'
-import counterReducer from '../features/counter/counterSlice'
 
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    counter: counterReducer
-  },
-});
+const rootReducer = combineReducers({
+  weightsReducer,
+  setHeightReducer,
+  setAgeReducer,
+  setSexReducer,
+  setNameReducer,
+  setActivityLevelReducer,
+  setGoalReducer,
+  userReducer
+})
+
+export const store = createStore(rootReducer)
+console.log(store.getState())
