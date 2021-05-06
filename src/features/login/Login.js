7189f44dde903a0c175f49b1bce70792bea69e65
kudryk <kudryk@kth.se>
2021-05-06 15:13:48 +0200

    Finished project

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -1,13 +1,13 @@
+import firebase from 'firebase';
 import {
     setActiveUser,
     selectUserName,
 } from './userSlice';
-import {
-    start
-  } from '../main/mainSlice';
 import {useDispatch, useSelector} from 'react-redux';
 import {auth, provider} from '../../firebase/firebase';
-import styles from './Login.module.css';
+import styles from './Login.css';
+import pic from './app.png'
+import { start } from '../main/mainSlice';
 
 export function Login() {
     const dispatch = useDispatch();
@@ -20,11 +20,49 @@ export function Login() {
             userName: result.user.displayName,
             userEmail: result.user.email,
             userId: result.user.uid
-        })) 
+        }))
+        var check = false;
+        var current = new Date().getDay();
+        firebase.database().ref().child("users/" + result.user.uid + "/steps").once("value")
+        .then(function(snapshot) {
+          check = snapshot.hasChild(current.toString());
+        });
+        const datum = new Date().toLocaleDateString('zh-Hans-CN');
+        if(!check){
+          firebase.database().ref().child("users/" + result.user.uid + "/steps/" + datum).set(0);
+        }
+        dispatch(start())
         window.location.hash="main";
         })
     }
     return (
-            <button className={styles.login} onClick = {handleSignIn}>Sign In</button>
+        <div>
+            <div class="split left">
+                <div class="centered">
+                    <h1 class="logInTxt">Welcome! &#128522;</h1>
+                    <button class="logInBtn"  onClick = {handleSignIn}>Sign In</button>
+                </div>
+            </div>
+            <div class="split right"> 
+                <div class="centered">
+                    <div class="infoTxt">
+                        <h3 >Hello user! &#127939;</h3>
+                        <h4 class="mainTxt">Want to track your steps, get in shape or just maintain a healthy lifestyle?
+                            Then this app is for you. We are health4life, a team of KTH students who developed an aplication
+                            for everyone, no matter your current health status. In this app you will be able to:
+                        <ul>
+                            <li>Track your steps in real time</li>
+                            <li>See your progress in your health journey</li>
+                            <li>Track clalorie intake, weight loss, steps per day etc.</li>
+                            <li>Invite your friends to workouts</li>
+                        </ul>
+                            If this sounds like something for you, we welcome you to join the community!
+                        </h4>
+                        <h3>Good luck on your health journey!</h3>
+                    </div>
+                    <img class="logoImg" src={pic}  alt="logo"></img>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/features/main/Main.js
+++ src/features/main/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './Main.module.css';
 import {
@@ -19,14 +19,14 @@ const Main = () => {
     const dispatch = useDispatch();
    const split = uName.split(" ");
    const name = split[0]; 
+   console.log(uid);
     var current = new Date().toLocaleDateString('zh-Hans-CN');
-    if(count != 0){
-        firebase.database().ref().child("users/" + uid + "/" + current).once('value',function(snap){
+    console.log(current);
+        firebase.database().ref().child("users/" + uid + "/steps/" + current).once('value',function(snap){
             if (snap){
               newValue= snap.val();
             }
-          }).then(()=> {myVar = setTimeout(() => {dispatch(increment(newValue, myVar))}, 1000)});
-    }
+          }).then(()=> {myVar = setTimeout(() => {dispatch(increment(newValue))}, 5000)});  
     
     return(
         <div className={styles.body}>
@@ -40,13 +40,21 @@ const Main = () => {
             className={styles.steps}
             aria-label="Increment value"
            
-            > Steps</button></div>
-            <button 
+            > Steps</button></div>  
+            <div>
+              <p>
+                <span className={styles.pulseValue}>170</span>
+                <span className={styles.calorieValue}> 2300</span>
+              </p>
+            </div>
+             <div>
+             <button 
             className={styles.pulse}
             >Pulse</button>
             <button 
             className={styles.calories}
             > Calories</button>
+             </div>
             </div>
             <div>
 
